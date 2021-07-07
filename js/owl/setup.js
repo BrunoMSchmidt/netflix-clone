f7e3670179ce798f4cf4c4d39c9e911b30d57765
Bruno M Schmidt <brunomschmidt1@gmail.com>
2021-07-07 04:22:11 -0300

    Projeto finalizado

---
+++ js/owl/setup.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    420: {
+      items: 2,
+    },
+    600: {
+      items: 4,
+    },
+    900: {
+      items: 6
+    },
+  },
+});
